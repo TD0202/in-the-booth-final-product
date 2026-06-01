8b9419b6cec7873d0b5c1fd48b6946ab647bacad
TD0202 <tdavis212398@gmail.com>
2026-06-01 14:22:20 -0600

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+In The Booth Website Package - Table of Contents
+
+1. index.html - Home page
+2. venues.html - Venue partnership page
+3. events.html - Events and parties page
+4. packages.html - Packages and pricing page
+5. gallery.html - Gallery page
+6. booking.html - Booking inquiry page
+7. contact.html - Contact page
+8. assets/css/styles.css - Website styling
+9. assets/js/main.js - Booking form demo script
+10. README.md - Website instructions
